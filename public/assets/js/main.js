c088de940e8b0eb71258c6d9e5b974212e7135c9
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-04-29 14:23:23 +0530

    passing turns to different players and displaying data

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,35 +1,43 @@
 let socket = io();
-let diceNumber = {
-    number: 0,
-    turn: true
-}
+let diceNumber
+// let diceNumber = {
+//     number: 0,
+//     turn: true
+// }
 document.getElementById("btn").addEventListener('click', function(event){
-    diceNumber.number = Math.round(Math.random() * 6)
-    console.log('changed')
-    diceNumber.turn = false
+    diceNumber = Math.round(Math.random() * 6)
+    // console.log('changed')
+    // diceNumber.turn = false
+    socket.emit('pass_turn',diceNumber)
+    // socket.emit()
 })
 
-
-socket.emit('new player');
-
-setInterval(function(){
-    socket.emit('diceNumber', diceNumber);
-}, 1000/60)
-
-
-socket.on('state', function(players){
-
-    for(let id in players){
-        // console.log(players[id])
-        let player = players[id]
-        if(player.turn == false){
-            document.getElementById("dice-number").innerHTML = `${player.num}`
-            document.getElementById("turn").innerHTML = `your turn`
-        }
-        if( player.turn == true){
-            document.getElementById("turn").innerHTML = `Waiting for opponent`
-            socket.emit('diceNumber', diceNumber.turn = false)
-        }
+socket.on('state', function(dValues){
+    for(let i in dValues){
+        document.getElementById("dice-number").innerHTML = dValues[i]
     }
-
 })
+
+// socket.emit('new player');
+
+// setInterval(function(){
+//     socket.emit('diceNumber', diceNumber);
+// }, 1000/60)
+
+
+// socket.on('state', function(players){
+
+//     for(let id in players){
+//         // console.log(players[id])
+//         let player = players[id]
+//         if(player.turn == false){
+//             document.getElementById("dice-number").innerHTML = `${player.num}`
+//             document.getElementById("turn").innerHTML = `your turn`
+//         }
+//         if( player.turn == true){
+//             document.getElementById("turn").innerHTML = `Waiting for opponent`
+//             socket.emit('diceNumber', diceNumber.turn = false)
+//         }
+//     }
+
+// })
--- server.js
+++ server.js
@@ -15,22 +15,85 @@ server.listen(8000, function() {
     console.log('Starting server on port 8000');
   });
 
-let players = {}
-// Adding websocket handler
+let players = []
+let dValues = []
+let currentTurn = 0
+let timeOut
+let turn = 0
+let value = 0
+const MaxWaiting = 5000
+
+function nextTurn(){
+    turn = currentTurn++ % players.length;
+    players[turn].emit('your_turn')
+    console.log("Next player turn has been triggered", turn)
+    triggerTimeout()
+}
+
+function triggerTimeout(){
+    timeOut = setTimeout(() =>{
+        autoMove()
+        nextTurn()
+    }, MaxWaiting)
+}
+
+function autoMove(){
+    let diceNumber = Math.round(Math.random() * 6)
+    dValues.push(diceNumber)
+    console.log(dValues)
+    giveData()
+}
+
+function resetTimeOut(){
+    if(typeof timeOut === 'object'){
+        console.log("timeout been reset")
+        clearTimeout(timeOut)
+    }
+}
+
 io.on('connection', function(socket){
-    socket.on('new player', function(){
-        console.log("New player has connected")
-        players[socket.id] = {
-            num: 0,
-            turn: true
-        };
-    });
-    socket.on('diceNumber', function(data){
-        let player = players[socket.id] || {};
-        player.num = data.number,
-        player.turn = data.turn
-    });
-});
-setInterval(function(){
-    io.sockets.emit('state', players);
-}, 1000/60)
+    console.log("New player has connected")
+
+    players.push(socket);
+    socket.on('pass_turn', function(data){
+        if(players[turn] == socket){
+            dValues.push(data)
+            console.log(dValues)
+            resetTimeOut();
+            giveData()
+            nextTurn();
+        }
+    })
+    socket.on('disconnect', function(){
+        console.log('A player disconnected');
+        players.splice(players.indexOf(socket),1);
+        turn--;
+        console.log("A number of players now ",players.length);
+      });
+})
+function giveData(){
+    io.sockets.emit('state', dValues);
+}
+
+// let players = {}
+
+// io.on('connection', function(socket){
+//     socket.on('new player', function(){
+//         console.log("New player has connected")
+//         players[socket.id] = {
+//             num: 0,
+//             turn: true
+//         };
+//     });
+//     socket.on('diceNumber', function(data){
+//         let player = players[socket.id] || {};
+//         player.num = data.number,
+//         player.turn = data.turn
+//     });
+// });
+// io.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// setInterval(function(){
+//     io.sockets.emit('state', players);
+// }, 1000/60)
